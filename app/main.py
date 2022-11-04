c6bcd7ef9fc1dc59942d1f77719847c10d86d895
RobbeBreens <robbe.breens@student.howest>
2022-11-04 12:46:56 +0100

    feat: add inference endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+from tensorflow.keras import preprocessing
+from PIL import Image
+
 
 import numpy as np
 
@@ -19,5 +22,17 @@ async def index():
 async def UploadImage(file: bytes = File(...)):
     with open('image.jpg','wb') as image:
         image.write(file)
+        image = Image.open("./image.jpg") 
+
+        test_image = image.resize((180,180))
+        test_image = preprocessing.image.img_to_array(test_image)
+        test_image = test_image / 255.0
+        test_image = np.expand_dims(test_image, axis=0)
+
+        class_names = {0 : 'background', 1 :'litter'}    
+        predictions = MODEL.predict(test_image)
+        scores = tf.nn.softmax(predictions[0])
+        scores = scores.numpy()
         image.close()
-    return 'got it'
+    
+    return f"{class_names[np.argmax(scores)]} with a { (100 *       np.max(scores)).round(2) } % confidence."
